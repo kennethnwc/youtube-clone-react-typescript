c1e2f584ed58236a1e8581be5081b401467251dc
Kenneth NG <nwc27101994@yahoo.com.hk>
2020-06-02 14:21:35 +0800

    Display video

--- src/components/VideoDetail.tsx
+++ src/components/VideoDetail.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 
-interface Props {}
-const VideoDetail: React.FC<Props> = () => {
-  return <div>Video Detail</div>;
+import { Paper, Typography } from "@material-ui/core";
+
+interface Props {
+  video: {
+    id: { videoId: string };
+    snippet: { title: string; channelTitle: string; description: string };
+  };
+}
+const VideoDetail: React.FC<Props> = ({ video }) => {
+  if (!video) return <div>Loading...</div>;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <>
+      <Paper elevation={6} style={{ height: "70%" }}>
+        <iframe
+          frameBorder="0"
+          height="100%"
+          width="100%"
+          title="Video Player"
+          src={videoSrc}
+        />
+      </Paper>
+      <Paper elevation={6} style={{ padding: "15px" }}>
+        <Typography variant="h4">
+          {video.snippet.title} - {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle1">
+          {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle2">{video.snippet.description}</Typography>
+      </Paper>
+    </>
+  );
 };
 
 export default VideoDetail;
