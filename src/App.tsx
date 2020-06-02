45fb266843da254cc8eb1ffc391e5a523b5594aa
Kenneth NG <nwc27101994@yahoo.com.hk>
2020-06-02 12:38:30 +0800

    Fixed axios and successfully fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import * as React from "react";
 import { Grid } from "@material-ui/core";
+import * as React from "react";
 import youtube from "./api/youtube";
-
-import { SearchBar, VideoList, VideoDetail } from "./components";
+import { SearchBar, VideoDetail, VideoList } from "./components";
+import apiKey from "./api/apiKey";
 
 interface Props {}
 
 export const App: React.FC<Props> = () => {
+  const handleSubmit = async (searchTerm: string) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: apiKey,
+        q: searchTerm,
+      },
+    });
+
+    console.log(response);
+  };
+
   return (
     <Grid justify="center" container spacing={10}>
       <Grid item xs={12}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
-            <SearchBar />
+            <SearchBar onFormSubmit={handleSubmit} />
           </Grid>
           <Grid item xs={8}>
             <VideoDetail />
--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
-import apiKey from "./apiKey";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: apiKey,
-  },
 });
